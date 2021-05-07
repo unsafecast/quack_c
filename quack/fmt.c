cc0ba889cb9db101c68b9b3515311f422ec78b7e
unsafecast <unsafecast@service>
2021-05-07 18:47:11 +0300

    fixed a bug where qkFmtPrintString wouldn't reset the format

--- quack/fmt.c
+++ quack/fmt.c
@@ -8,10 +8,14 @@
 
 #define RESET "\x1b[0m"
 
-static void applyFormat(FILE* stream, i64 fmt) {
+inline static void applyFormat(FILE* stream, i64 fmt) {
     if (fmt & QK_CONS_FMT_RED)  fputs(COL_RED, stream);
     if (fmt & QK_CONS_FMT_BLUE) fputs(COL_BLUE, stream);
     if (fmt & QK_CONS_FMT_BOLD) fputs(BOLD, stream);
+}
+
+inline static void resetFormat(FILE* stream) {
+    fputs(RESET, stream);
 }
 
 void qkFmtPrintf(FILE* stream, i64 fmt, const char* str, ...) {
@@ -20,7 +24,7 @@ void qkFmtPrintf(FILE* stream, i64 fmt, const char* str, ...) {
 
     applyFormat(stream, fmt);
     vfprintf(stream, str, list);
-    fputs(RESET, stream);
+    resetFormat(stream);
 
     va_end(list);
 }
@@ -28,4 +32,5 @@ void qkFmtPrintf(FILE* stream, i64 fmt, const char* str, ...) {
 void qkFmtPrintString(FILE* stream, i64 fmt, const QkString* str) {
     applyFormat(stream, fmt);
     qkPrintString(str, stream);
+    resetFormat(stream);
 }
